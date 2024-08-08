1881c9749d93788f1a7eca2f4e70075e4f829765
Sachin kumar <kumarsachinkain2000@gmail.com>
2024-08-08 13:24:16 +0530

    heading icon marging change

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ photoURL }) => {
   return (
     <div className='header'>
       <div className="header__logo">
-        <img src="./src/assets/logo.jpg" alt="Drive_logo" />
+        <img src="\src\Component\Header\LOGO.png" alt="Drive_logo" />
         <span>Drive</span>
       </div>
       <div className="header__search">
